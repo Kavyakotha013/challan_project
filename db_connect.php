4f6ec412ce7525fc4d9783ae3fd1a26e0d9ad7a4
Kavyakotha013 <155290440+Kavyakotha013@users.noreply.github.com>
2026-04-07 00:47:16 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,13 @@
 <?php
-$host = "mysql-ifj.railway.internal";
-$user = "root";
-$pass = "OqBOdvXusaRyGzBoZbGVQLSrUeQgoRtt";
-$db   = "railway";
-$port = 3306;
+$host = $_ENV['MYSQLHOST'];
+$user = $_ENV['MYSQLUSER'];
+$pass = $_ENV['MYSQLPASSWORD'];
+$db   = $_ENV['MYSQLDATABASE'];
+$port = $_ENV['MYSQLPORT'];
 
-$conn = mysqli_init();
-mysqli_real_connect($conn, $host, $user, $pass, $db, $port);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
